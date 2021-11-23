ae7669fcd0234f9612c9dc60d4524613db20b515
675785_Priyanka <twinkledahale98@gmail.com>
2021-11-23 09:37:58 +0000

    Saving changes from StackRoute workbench on Tue Nov 23 2021 15:07:56 GMT+0530 (India Standard Time)

--- ArrayListRoute.java
+++ ArrayListRoute.java
@@ -48,7 +48,7 @@ public class ArrayListRoute
 
     public void displayList()
     {
-       routesInfo.forEach(display);
+       routeInfo.forEach(display);
     }
 
     public void showDirectFlights(String source)
