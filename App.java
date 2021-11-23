57542df66cc79a39bb3ad1a3e2821bf55dc6d740
675785_Priyanka <twinkledahale98@gmail.com>
2021-11-23 07:27:58 +0000

    Saving changes from StackRoute workbench on Tue Nov 23 2021 12:57:57 GMT+0530 (India Standard Time)

--- App.java
+++ App.java
@@ -3,6 +3,7 @@ public class App
     public static void main(String args[])
     {
         ArrayListRoute rl = new ArrayListRoute();
+        String filename="routes.csv";
         rl.readFromFile()
     }
 }
