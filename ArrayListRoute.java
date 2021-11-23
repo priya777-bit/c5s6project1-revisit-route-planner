f5aaa2b81643571119ba5ec41dd5ff607585df83
675785_Priyanka <twinkledahale98@gmail.com>
2021-11-23 09:55:03 +0000

    Saving changes from StackRoute workbench on Tue Nov 23 2021 15:25:02 GMT+0530 (India Standard Time)

--- ArrayListRoute.java
+++ ArrayListRoute.java
@@ -68,7 +68,7 @@ public class ArrayListRoute
 
     public void sortDirectFlights(List<Route> directFlight)
     {
-        directFlight.stream().sorted()
+        directFlight.stream().sorted(Comparator.comparing(Route::getDestination))
     }
 
 }
