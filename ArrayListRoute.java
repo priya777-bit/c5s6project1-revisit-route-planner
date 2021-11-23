14e1ff9f22b0d6901befe2fcd84f55f1860634fd
675785_Priyanka <twinkledahale98@gmail.com>
2021-11-23 05:56:06 +0000

    Saving changes from StackRoute workbench on Tue Nov 23 2021 11:26:04 GMT+0530 (India Standard Time)

--- ArrayListRoute.java
+++ ArrayListRoute.java
@@ -12,7 +12,7 @@ public class ArrayListRoute
 {
     List<Route> routeInfo = new ArrayList<Route>();
     Optional<String> directFlightInfo = Optional.empty();
-    Optional
+    Optional<String> sortDirectFlights = Optional.empty();
 
     public List<Route> readFromFile(String filename)
     {
