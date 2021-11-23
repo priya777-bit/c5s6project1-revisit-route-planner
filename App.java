d060332fcc20747e061121fe8ce53b4656a7b557
675785_Priyanka <twinkledahale98@gmail.com>
2021-11-23 08:37:55 +0000

    Saving changes from StackRoute workbench on Tue Nov 23 2021 14:07:54 GMT+0530 (India Standard Time)

--- App.java
+++ App.java
@@ -5,11 +5,12 @@ public class App
     public static void main(String args[])
     {
         ArrayListRoute rl = new ArrayListRoute();
+        Scanner sc = new Scanner(System.in);
         String filename="routes.csv";
         rl.readFromFile(filename);
         rl.displayList();
         List<Route> flightlist = new ArrayList<Route>();
-        String sourceCity=
-        flightlist=rl.showDirectFlights(flightlist);
+        String sourceCity=sc.nextLine();
+        flightlist=rl.showDirectFlights(flightlist,sourceCity);
     }
 }
