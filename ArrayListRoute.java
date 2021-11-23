ff48526c78a0babbe7e18c450ef0c0d96549be96
675785_Priyanka <twinkledahale98@gmail.com>
2021-11-23 10:50:40 +0000

    Saving changes from StackRoute workbench on Tue Nov 23 2021 16:20:39 GMT+0530 (India Standard Time)

--- ArrayListRoute.java
+++ ArrayListRoute.java
@@ -79,7 +79,7 @@ public class ArrayListRoute
 
         if(sourcedestination.isPresent())
         {
-            Optional<Route> destination1 = routeInfo.stream().filter(p->(p.getDestination().equalsIgnoreCase(destination).map(f->f.getDestination()).findAny();
+            Optional<Route> destination1 = routeInfo.stream().filter(p->(p.getDestination().equalsIgnoreCase(destination).findAny();
             
             if(destination1.isPresent())
             {
