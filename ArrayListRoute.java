e56f7510c4583fce5a6535876d2234f9745044f1
675785_Priyanka <twinkledahale98@gmail.com>
2021-11-23 04:52:09 +0000

    Saving changes from StackRoute workbench on Tue Nov 23 2021 10:22:08 GMT+0530 (India Standard Time)

--- ArrayListRoute.java
+++ ArrayListRoute.java
@@ -14,7 +14,7 @@ public class ArrayListRoute
         {
             BufferedReader br = new BufferedReader(new FileReader(filename));
 
-            
+            routeList.add(new Route())
         }
     }
 }
