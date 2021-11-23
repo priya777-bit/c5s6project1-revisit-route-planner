b2a696589df647bdcd123356cf695cf0f5212695
675785_Priyanka <twinkledahale98@gmail.com>
2021-11-23 12:46:31 +0000

    Saving changes from StackRoute workbench on Tue Nov 23 2021 18:16:30 GMT+0530 (India Standard Time)

--- ArrayListRoute.java
+++ ArrayListRoute.java
@@ -16,6 +16,7 @@ public class ArrayListRoute
     Optional<Route> directFlightInfo = Optional.empty();
     List<Route> directFlight = new ArrayList<Route>();
     List<Route> showAll = new ArrayList<Route>();
+    String tempdestination="";
 
     public List<Route> readFromFile(String filename)
     {
